cfe18e8bdda36b1504b236096e1931eb0f1ab32a
haa40 <haa40@sfu.ca>
2019-10-31 07:45:39 -0800

    added basic map-reading in startGame.js. Fixed some error in the testMap.json file.

--- static/objects/jsonProcessor.js
+++ static/objects/jsonProcessor.js
@@ -1,1 +1,15 @@
 //read and construct the elements in the .json data file for the map.
+/*
+//Reading json file.
+//this is copy-pasted from https://medium.com/@osiolabs/read-write-json-files-with-node-js-92d03cc82824
+const fs = require('fs')
+var mapData;
+
+fs.readFile('./testMap.json', 'utf8', (err, mapDataString) => {
+    if (err) {
+        console.log("File read failed:", err);
+        return;
+    }
+    mapData = JSON.parse(mapDataString);
+    console.log(JSON.stringify(mapData));
+})*/
--- static/startGame.js
+++ static/startGame.js
@@ -9,6 +9,22 @@ fuction startGame(){
 
 //create map, element placements, player info
 function initGame(){
+  //Reading json file.
+  //The whole map data is read from JSON file and saved to 'mapData'.
+  //this is copy-pasted from https://medium.com/@osiolabs/read-write-json-files-with-node-js-92d03cc82824
+  const fs = require('fs')
+  var mapData;
+  //The link to JSON file is currently set to 'testMap'. When implementing map-choosing feature, modify the file link.
+  fs.readFile('static/objects/testMap.json', 'utf8', (err, mapDataString) => {
+      if (err) {
+          console.log("File read failed:", err);
+          return;
+      }
+      mapData = JSON.parse(mapDataString);
+      //console.log(JSON.stringify(mapData)); //will show the whole map data in console.
+  })
+
+  //TODO: create functions to construct map variables. Hailey will do this!
 
 }
 
