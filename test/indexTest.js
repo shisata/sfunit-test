2d7ead6cd3c1899010a7e169137f576e711a7870
fwarraic <fwarraic@sfu.ca>
2019-11-15 22:41:31 -0800

    formatted the test cases a bit properly

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,26 +1,26 @@
 const assert = require('chai').assert;
-const sayHello = require('../index').sayHello;
-const addNumbers = require('../index').addNumbers;
-//const index = require('../index');
+//const sayHello = require('../index').sayHello;
+//const addNumbers = require('../index').addNumbers;
+const index = require('../index');
 
 describe('Index', function(){
     it('sayHello should return hello', function(){
-        let result = sayHello();
+        let result = index.sayHello();
         assert.equal(result, 'hello');
     });
 
     it('sayHello should return type string', function(){
-        let result = sayHello();
+        let result = index.sayHello();
         assert.typeOf(result, 'string');
     });
 
     it('addNumbers should be above 5', function(){
-        let result = addNumbers(5, 5);
+        let result = index.addNumbers(5, 5);
         assert.isAbove(result, 5);
     });
 
     it('addNumbers should return type number', function(){
-        let result = addNumbers(5, 5);
+        let result = index.addNumbers(5, 5);
         assert.typeOf(result, 'number');
     });
 });
