3ebfc94b59ae3ce8297ea25692faf95b077284ae
fwarraic <fwarraic@sfu.ca>
2019-11-15 22:50:56 -0800

    grouped test cases according to their respective functions

--- test/indexTest.js
+++ test/indexTest.js
@@ -8,23 +8,35 @@ sayHelloResult = index.sayHello();
 addNumbersResult = index.addNumbers(5, 5);
 
 describe('Index', function(){
-    it('sayHello should return hello', function(){
-        //let result = index.sayHello();
-        assert.equal(sayHelloResult, 'hello');
+
+    // Test cases for sayHello function
+    describe('sayHello()', function(){
+        
+        it('sayHello should return hello', function(){
+            //let result = index.sayHello();
+            assert.equal(sayHelloResult, 'hello');
+        });
+    
+        it('sayHello should return type string', function(){
+            //let result = index.sayHello();
+            assert.typeOf(sayHelloResult, 'string');
+        });
+
     });
 
-    it('sayHello should return type string', function(){
-        //let result = index.sayHello();
-        assert.typeOf(sayHelloResult, 'string');
-    });
+    // Test cases for addNumbers function
+   describe('addNumbers()', function(){
 
-    it('addNumbers should be above 5', function(){
-        //let result = index.addNumbers(5, 5);
-        assert.isAbove(addNumbersResult, 5);
-    });
+        it('addNumbers should be above 5', function(){
+            //let result = index.addNumbers(5, 5);
+            assert.isAbove(addNumbersResult, 5);
+        });
 
-    it('addNumbers should return type number', function(){
-        //let result = index.addNumbers(5, 5);
-        assert.typeOf(addNumbersResult, 'number');
-    });
+        it('addNumbers should return type number', function(){
+            //let result = index.addNumbers(5, 5);
+            assert.typeOf(addNumbersResult, 'number');
+        });
+
+   });
+
 });
