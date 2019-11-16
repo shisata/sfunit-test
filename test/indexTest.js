51f50e4d3bc081fd113196f4bd6e9edb29b4fa17
fwarraic <fwarraic@sfu.ca>
2019-11-15 22:45:46 -0800

    refactored indexTest.js

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,24 +3,28 @@ const assert = require('chai').assert;
 //const addNumbers = require('../index').addNumbers;
 const index = require('../index');
 
+// Results
+sayHelloResult = index.sayHello();
+addNumbersResult = index.addNumbers(5, 5);
+
 describe('Index', function(){
     it('sayHello should return hello', function(){
-        let result = index.sayHello();
-        assert.equal(result, 'hello');
+        //let result = index.sayHello();
+        assert.equal(sayHelloResult, 'hello');
     });
 
     it('sayHello should return type string', function(){
-        let result = index.sayHello();
-        assert.typeOf(result, 'string');
+        //let result = index.sayHello();
+        assert.typeOf(sayHelloResult, 'string');
     });
 
     it('addNumbers should be above 5', function(){
-        let result = index.addNumbers(5, 5);
-        assert.isAbove(result, 5);
+        //let result = index.addNumbers(5, 5);
+        assert.isAbove(addNumbersResult, 5);
     });
 
     it('addNumbers should return type number', function(){
-        let result = index.addNumbers(5, 5);
-        assert.typeOf(result, 'number');
+        //let result = index.addNumbers(5, 5);
+        assert.typeOf(addNumbersResult, 'number');
     });
 });
